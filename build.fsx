bb26b33a0f1b2828292111c87ec58eb41db58b85
Drew Seward <awswrd@gmail.com>
2017-07-16 12:46:06 -0700

    Add paket template, remove most of build.fsx

--- build.fsx
+++ build.fsx
@@ -3,40 +3,12 @@
 
 open Fake
 
-// Directories
-let buildDir  = "./build/"
-let deployDir = "./deploy/"
+let projects = !! "/**/*.fsproj"
 
-
-// Filesets
-let appReferences  =
-    !! "/**/*.csproj"
-    ++ "/**/*.fsproj"
-
-// version info
-let version = "0.1"  // or retrieve from CI server
-
-// Targets
-Target "Clean" (fun _ ->
-    CleanDirs [buildDir; deployDir]
-)
-
-Target "Build" (fun _ ->
-    // compile all projects below src/app/
-    MSBuildDebug buildDir "Build" appReferences
+Target "Build:Release" (fun _ ->
+    projects
+    |> MSBuildRelease null "Clean;Rebuild"
     |> Log "AppBuild-Output: "
 )
 
-Target "Deploy" (fun _ ->
-    !! (buildDir + "/**/*.*")
-    -- "*.zip"
-    |> Zip buildDir (deployDir + "ApplicationName." + version + ".zip")
-)
-
-// Build order
-"Clean"
-  ==> "Build"
-  ==> "Deploy"
-
-// start build
-RunTargetOrDefault "Build"
+RunTargetOrDefault "Build:Release"
