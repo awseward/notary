81bed8e70ada0f5646be4b2d61d194324eb9b70f
Drew Seward <awswrd@gmail.com>
2017-07-16 10:44:04 -0700

    Increment patch -> 0.0.1

--- src/Notary/AssemblyInfo.fs
+++ src/Notary/AssemblyInfo.fs
@@ -13,6 +13,6 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyVersion("0.0.1.0")>]
 [<assembly: AssemblyFileVersion("0.0.1.0")>]
-[<assembly: AssemblyInformationalVersion("0.0.1-a")>]
+[<assembly: AssemblyInformationalVersion("0.0.1")>]
 do
     ()
