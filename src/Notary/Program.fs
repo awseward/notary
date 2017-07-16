ccadad3731dd8d497e6ad3c7e8def9675613573d
Drew Seward <awswrd@gmail.com>
2017-07-15 17:43:13 -0700

    Implement print, ?detect?
    Detect still needs to be verified.

--- src/Notary/Program.fs
+++ src/Notary/Program.fs
@@ -1,82 +1,127 @@
 open Argu
 open Notary
 open System
-let private _basicFail () =
-    printfn "TODO: Usage"
-    1
-let private _block = Console.ReadLine >> ignore
 
 type DetectArgs =
-    | SignTool of path:string
+| [<Mandatory>] Pfx of ``pfx filepath``:string
+| [<MainCommand; ExactlyOnce; Last>] Files of ``files to check``:string list
 with
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | SignTool _ -> "Specify the path to your signtool.exe"
-
+            | Pfx _ -> "pfx filepath"
+            | Files _ -> "files which will be checked for having been signed by the given pfx file"
+and PrintArgs =
+| [<MainCommand; ExactlyOnce; Last>] Pfx of ``pfx filepath``:string
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Pfx _ -> "pfx filepath"
+and SignArgs =
+| [<Mandatory>] Pfx of ``pfx filepath``:string
+| [<Mandatory>] Password of string
+| [<MainCommand; ExactlyOnce; Last>] Files of ``files to check``:string list
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Pfx _ -> "pfx filepath"
+            | Password _ -> "password for pfx file"
+            | Files _ -> "files which will be checked for having been signed by the given pfx file"
 and NotaryArgs =
-    | [<CliPrefix(CliPrefix.None)>] Detect of ParseResults<DetectArgs>
+| [<CliPrefix(CliPrefix.None)>] Detect of ParseResults<DetectArgs>
+| [<CliPrefix(CliPrefix.None)>] Print of ParseResults<PrintArgs>
+| [<CliPrefix(CliPrefix.None)>] Sign of ParseResults<SignArgs>
+| [<Inherit>] Certutil of path:string
+| [<Inherit>] Signtool of path:string
 with
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | Detect _ -> "Detect whether a file is already signed by the specified pfx file"
+            | Detect _ -> "detect whether a file is already signed by the specified pfx file"
+            | Print _ -> "print the sha1 hash of a given pfx certificate file"
+            | Sign _ -> "sign files with the given pfx certificate only if they're not already signed by that same certificate"
+            | Certutil _ -> "certutil.exe filepath"
+            | Signtool _ -> "signtool.exe filepath"
 
 [<EntryPoint>]
 let main argv =
     let parser = ArgumentParser.Create<NotaryArgs>()
+    // TODO: Unhardcode these
+    let certutil = @"C:\WINDOWS\System32\certutil.exe"
+    let signtool = @"C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x64\signtool.exe"
 
-    parser.PrintUsage()
-    |> printfn "%s"
+    try
+        let parseResults = parser.Parse argv
+        match parseResults.TryGetSubCommand() with
+        | Some (Detect args) ->
+            // TODO: Double check that this case works ======================================================
 
-    printfn "wat"
-
-    printfn "argv: %A" argv
-
-    // TODO: Put proper CLI parsing in place
-    if Array.isEmpty argv then _basicFail()
-    else
-        // TODO: Unhardcode these
-        let certutil = @"C:\WINDOWS\System32\certutil.exe"
-        let signtool = @"C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x64\signtool.exe"
-
-        try
-            match Array.head argv with
-            | "getPfxCertHash" ->
-                let pfx = argv.[1]
-
-                pfx
-                |> Lib.getPfxCertHash certutil
-                |> printfn "%s"
-
-                _block()
-                0
-            | "checkIfAlreadySigned" ->
-                let pfx  = argv.[1]
-                let file = argv.[2]
+            match args.TryGetResult <@ DetectArgs.Pfx @>, args.TryGetResult <@ DetectArgs.Files @> with
+            | (Some pfx, Some files) ->
+                // TODO: Fix to be a single file
+                let file = Seq.head files
 
                 file
                 |> Lib.isFileSignedByPfx signtool certutil pfx
                 |> printfn "%b"
 
-                _block()
-                0
-            | "ensureSigned" ->
-                let pfx       = argv.[1]
-                let password  = argv.[2]
-                let filePaths =
-                    argv
-                    |> Array.skip 3
-                    |> Array.map (fun str -> str.Trim())
-
-                Lib.signIfNotSigned signtool certutil pfx password filePaths
-
-                _block()
                 0
             | _ ->
-                _basicFail()
-        with
-        | ex ->
-            printfn "%s" ex.Message
-            _block()
-            _basicFail()
+                parser.PrintUsage()
+                |> printfn "%s"
+                1
+
+            // ----------------------------------------------------------------------------------------------
+        | Some (Print args) ->
+            match args.TryGetResult <@ PrintArgs.Pfx @> with
+            | Some pfx ->
+                pfx
+                |> Lib.getPfxCertHash certutil
+                |> printfn "%s"
+                0
+            | None ->
+                parser.PrintUsage()
+                |> printfn "%s"
+                1
+        | Some (Sign args) ->
+            printfn "Args: %A (Sign)" args      
+            0  
+        | _ ->
+            parser.PrintUsage()
+            |> printfn "%s"
+            1
+    with
+    | :? ArguParseException as ex ->
+        printfn "%s" ex.Message
+        1
+
+    // // TODO: Put proper CLI parsing in place
+    // if Array.isEmpty argv then _basicFail()
+    // else
+    //     // TODO: Unhardcode these
+    //     let certutil = @"C:\WINDOWS\System32\certutil.exe"
+    //     let signtool = @"C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x64\signtool.exe"
+
+    //     try
+    //         match Array.head argv with
+    //         | "ensureSigned" ->
+    //             let pfx       = argv.[1]
+    //             let password  = argv.[2]
+    //             let filePaths =
+    //                 argv
+    //                 |> Array.skip 3
+    //                 |> Array.map (fun str -> str.Trim())
+
+    //             Lib.signIfNotSigned signtool certutil pfx password filePaths
+
+    //             _block()
+    //             0
+    //         | _ ->
+    //             _basicFail()
+    //     with
+    //     | ex ->
+    //         printfn "%s" ex.Message
+    //         _block()
+    //         _basicFail()
