62aab848f04280e6d2f219354d344b10b06abba4
Drew Seward <awswrd@gmail.com>
2017-09-04 13:46:49 -0700

    Pre-increment patch -> 0.1.1-a

--- src/Notary/AssemblyInfo.fs
+++ src/Notary/AssemblyInfo.fs
@@ -12,8 +12,8 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("a6e9d01d-7f96-4535-808f-a6d32436a3d1")>]
 
-[<assembly: AssemblyVersion("0.1.0.0")>]
-[<assembly: AssemblyFileVersion("0.1.0.0")>]
-[<assembly: AssemblyInformationalVersion("0.1.0")>]
+[<assembly: AssemblyVersion("0.1.1.0")>]
+[<assembly: AssemblyFileVersion("0.1.1.0")>]
+[<assembly: AssemblyInformationalVersion("0.1.1-a")>]
 do
     ()
