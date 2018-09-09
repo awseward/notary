e405138dbbbdb1bf0068810d9dfb0d5908b01ad3
Andrew Seward <awswrd@gmail.com>
2018-09-09 03:41:24 -0700

    chore: Reduce indent level to 2 spaces
    Going to be doing these as files get touched.
    
    Not excited to blow more meaningful commits away from blame, but such is
    life.

--- src/Notary/Args.fs
+++ src/Notary/Args.fs
@@ -9,32 +9,32 @@ module Args =
     | [<Mandatory>] Password of string
     | [<MainCommand; ExactlyOnce; Last>] File of ``file to check``:string
     with
-        interface IArgParserTemplate with
-            member this.Usage =
-                match this with
-                | Pfx _ -> "pfx filepath"
-                | Password _ -> "password for pfx file"
-                | File _ -> "file which will be checked for having been signed by the given pfx file"
+      interface IArgParserTemplate with
+        member this.Usage =
+          match this with
+          | Pfx _ -> "pfx filepath"
+          | Password _ -> "password for pfx file"
+          | File _ -> "file which will be checked for having been signed by the given pfx file"
     and PrintArgs =
     | [<Mandatory>] Password of string
     | [<MainCommand; ExactlyOnce; Last>] Pfx of ``pfx filepath``:string
     with
-        interface IArgParserTemplate with
-            member this.Usage =
-                match this with
-                | Password _ -> "password for pfx file"
-                | Pfx _ -> "pfx filepath"
+      interface IArgParserTemplate with
+        member this.Usage =
+          match this with
+          | Password _ -> "password for pfx file"
+          | Pfx _ -> "pfx filepath"
     and SignArgs =
     | [<Mandatory>] Pfx of ``pfx filepath``:string
     | [<Mandatory>] Password of string
     | [<MainCommand; ExactlyOnce; Last>] Files of ``files to check``:string list
     with
-        interface IArgParserTemplate with
-            member this.Usage =
-                match this with
-                | Pfx _ -> "pfx filepath"
-                | Password _ -> "password for pfx file"
-                | Files _ -> "files which will be checked for having been signed by the given pfx file"
+      interface IArgParserTemplate with
+        member this.Usage =
+          match this with
+          | Pfx _ -> "pfx filepath"
+          | Password _ -> "password for pfx file"
+          | Files _ -> "files which will be checked for having been signed by the given pfx file"
     and NotaryArgs =
     | [<CliPrefix(CliPrefix.None)>] Detect of ParseResults<DetectArgs>
     | [<CliPrefix(CliPrefix.None)>] Print of ParseResults<PrintArgs>
@@ -44,16 +44,16 @@ module Args =
     | [<Inherit; AltCommandLine("-v"); Hidden>] Verbose // TODO
     | [<Inherit; AltCommandLine("-q"); Hidden>] Quiet // TODO
     with
-        interface IArgParserTemplate with
-            member this.Usage =
-                match this with
-                | Detect _ -> "detect whether a file is already signed by the specified pfx file"
-                | Print _ -> "print the sha1 hash of a given pfx certificate file"
-                | Sign _ -> "sign files with the given pfx certificate only if they're not already signed by that same certificate"
-                | Certutil _ -> "certutil.exe filepath"
-                | Signtool _ -> "signtool.exe filepath"
-                | Verbose -> "Print more detailed output to console"
-                | Quiet -> "Print no output to console"
+      interface IArgParserTemplate with
+        member this.Usage =
+          match this with
+          | Detect _ -> "detect whether a file is already signed by the specified pfx file"
+          | Print _ -> "print the sha1 hash of a given pfx certificate file"
+          | Sign _ -> "sign files with the given pfx certificate only if they're not already signed by that same certificate"
+          | Certutil _ -> "certutil.exe filepath"
+          | Signtool _ -> "signtool.exe filepath"
+          | Verbose -> "Print more detailed output to console"
+          | Quiet -> "Print no output to console"
 
     let getToolPaths (parseResults: ParseResults<NotaryArgs>) =
       Notary.Tools.buildPaths
