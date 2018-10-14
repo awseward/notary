f5100622031abd99501406406829d138a7fd0150
Andrew Seward <awswrd@gmail.com>
2018-10-14 00:32:03 -0700

    fix: Resolve almost all deprecation warnings

--- build.fsx
+++ build.fsx
@@ -1,44 +1,49 @@
 #r "./packages/build/FAKE/tools/FakeLib.dll"
 #r "./packages/build/ASeward.MiscTools/lib/net471/ASeward.MiscTools.dll"
+#load "./temp/shims.fsx"
 
 open ASeward.MiscTools
 open ASeward.MiscTools.Versioning
-open Fake
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.Globbing.Operators
+open Notary.Fake.Shims
 
-FakeTargetStubs.createVersionTargets Target getBuildParam ["src/Notary/AssemblyInfo.fs"]
+FakeTargetStubs.createVersionTargets Target Environment.environVar ["src/Notary/AssemblyInfo.fs"]
 
 Target
   ReleaseNotes.FakeTargetStubs.targetName
   (fun _ ->
     ReleaseNotes.FakeTargetStubs.printReleaseNotes
-      (getBuildParamOrDefault)
+      (Environment.environVarOrDefault)
       "awseward"
       "notary"
   )
 
-let projects = !! "/**/*.fsproj"
+let projects = !! "**/*.fsproj"
 
 Target "Build:Release" (fun _ ->
     projects
-    |> MSBuildRelease null "Clean;Rebuild"
-    |> Log "AppBuild-Output: "
+    |> MSBuild.runRelease id null "Clean;Rebuild"
+    |> Trace.logItems "AppBuild-Output: "
 )
 
 let paketOutputDir = ".dist"
 
 Target "Paket:Pack" (fun _ ->
-    FileHelper.CleanDir paketOutputDir
+    Shell.cleanDir paketOutputDir
 
-    Paket.Pack <| fun p ->
+    Paket.pack <| fun p ->
         { p with
             OutputPath = paketOutputDir
         }
 )
 
 Target "Paket:Push" (fun _ ->
-    Paket.Push <| fun p ->
+    Paket.push <| fun p ->
         { p with
-            ApiKey = environVar "BUGSNAG_NET_NUGET_API_KEY"
+            ApiKey = Environment.environVar "BUGSNAG_NET_NUGET_API_KEY"
             WorkingDir = paketOutputDir
         }
 )
